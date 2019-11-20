3364bade4a490cf73e7dc06e8afc92ee9fc51da8
JsPractice <1912433492@qq.com>
2019-11-20 22:10:44 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+import "./App.css";
+const App = () => (
+  <div className="App">
+    <div className="AppGroup">
+      <h1>
+        Learn to <br />
+        design and code React apps
+      </h1>
+      <p>
+        Complete courses about the best tools and design systems. Prototype and
+        build apps with React and Swift.
+      </p>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
